7f7d1da900164666401629ec56a9544d2f45c554
Tomas Caraccia <tomascaraccia@gmail.com>
2020-03-09 20:18:51 -0300

    pre vuetify

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify';
+import lineclamp from './plugins/lineclamp';
+import moment from './plugins/moment';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
+  lineclamp,
+  moment,
   render: (h) => h(App),
 }).$mount('#app');
